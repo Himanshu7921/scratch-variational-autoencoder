3f68f04992062d8ef6e6273e4ecdb05628dc1f2f
Himanshu7921 <himanshusr451tehs@gmail.com>
2026-02-25 19:19:11 +0530

    chore: add base configuration file for hyperparams

--- src/config.py
+++ src/config.py
@@ -3,9 +3,9 @@ from dataclasses import dataclass
 @dataclass
 class Config:
     input_dim: int = 28 * 28
-    hidden_enc: int = 128
-    hidden_dec: int = 128
-    latent_dim: int = 64
+    hidden_enc: int = 256
+    hidden_dec: int = 256
+    latent_dim: int = 8
     batch_size: int = 128
     lr: float = 0.001
     epochs: int = 200
